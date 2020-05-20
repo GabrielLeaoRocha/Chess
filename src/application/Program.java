5ca7ba015e67d639e67d9cff6ac3d7dc2db11d34
GabrielLeaoRocha <gabrielleaorocha@gmail.com>
2020-05-20 19:12:46 -0300

    Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world!");
+		Position position = new Position(2, 4);
+		
+		System.out.println(position);
 	}
 
 }
