0bfcff31c1a4d564be87c856fd2c69a9929a71e2
GabrielLeaoRocha <gabrielleaorocha@gmail.com>
2020-05-21 10:50:11 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(2, 4);
+		Board board = new Board(8, 8);
 		
-		System.out.println(position);
 	}
 
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Board {
+
+	private int row;
+	private int column;
+	private Piece[][] pieces;
+	
+	//contructors
+	public Board(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+		pieces = new Piece[row][column];
+	}
+
+	//Getters and Setters
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	//methods
+	
+	
+}
