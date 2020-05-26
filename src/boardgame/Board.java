00284058cbbaacd204d37c0cfff90160d90eabbc
GabrielLeaoRocha <gabrielleaorocha@gmail.com>
2020-05-26 01:18:37 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,6 +47,19 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece (Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	public boolean positionExists(int row, int column) {
 		return row >= 0 && row < this.row && column >= 0 && column < this.column;
 	}
@@ -60,4 +73,6 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 }
